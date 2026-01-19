cc0cdcbb83a5013b5f66aa9078e85bcf0e4d3410
LeoXinhaoLee <Mc1114207918@outlook.com>
2026-01-19 15:15:01 +0000

    Update example script

--- run_mla_mi300x_local.sh
+++ run_mla_mi300x_local.sh
@@ -4,15 +4,5 @@ rm -rf ~/.triton  # rm cache
 rm -rf ~/.cache/torch_extensions  # rm cache
 
 cd gpu_mode
-# export CUSTOM_KERNEL_MODULE="mla-decode-local/MI300X_kernels/TTT_MLA.py"
-# python mla-decode-local/eval_local.py mla-decode-local/MI300X_kernels/TTT_MLA.out
-
-for i in 1 2 3; do
-# export CUSTOM_KERNEL_MODULE="mla-decode-local/MI300X_kernels/human_1st.py"
-# python mla-decode-local/eval_local.py mla-decode-local/MI300X_kernels/human_1st_${i}.out
-# export CUSTOM_KERNEL_MODULE="mla-decode-local/MI300X_kernels/human_2nd.py"
-# python mla-decode-local/eval_local.py mla-decode-local/MI300X_kernels/human_2nd_${i}.out
-# export CUSTOM_KERNEL_MODULE="mla-decode-local/MI300X_kernels/human_3rd.py"
-# python mla-decode-local/eval_local.py mla-decode-local/MI300X_kernels/human_3rd_${i}.out
-sleep 10
-done
+export CUSTOM_KERNEL_MODULE="mla-decode-local/MI300X_kernels/TTT_MLA.py"
+python mla-decode-local/eval_local.py mla-decode-local/MI300X_kernels/TTT_MLA.out
--- run_trimul_mi300x_local.sh
+++ run_trimul_mi300x_local.sh
@@ -7,5 +7,3 @@ cd gpu_mode
 export CUSTOM_KERNEL_MODULE="bioml/trimul/MI300X_kernels/TTT_MI300X.py"
 python bioml/trimul/eval_local.py bioml/trimul/MI300X_kernels/TTT_MI300X.out
 
-# export CUSTOM_KERNEL_MODULE="mla-decode-local/MI300X_kernels/human_1st.py"
-# python mla-decode-local/eval_local.py mla-decode-local/MI300X_kernels/human_1st.out
